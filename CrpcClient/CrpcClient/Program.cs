c83ad1bdd9498f8d5f62392a0087bb8f99d85012
Nisreen Wardeh <Nisreen.Wardeh@techprocess.net>
2024-01-10 16:15:29 +0300

    first commit

---
+++ CrpcClient/CrpcClient/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using GrpcClient;
+
+Console.WriteLine("press any key to cont...");
+Console.ReadLine();
+
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7000");
+var client = new Greeter.GreeterClient (channel);
+var reply = await client.SayHelloAsync(
+                     new HelloRequest { Name = "GreeterClient" });
+Console.WriteLine($"Greetings: " + reply.Message);
+Console.WriteLine("press any key to exit...");
+Console.ReadLine();
